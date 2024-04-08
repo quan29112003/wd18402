adca149624255340f29d5a098e306cb4533b344e
Nguyen Trong Duc <nguyentrongduc477@gmail.com>
2024-04-08 22:49:21 +0700

    Update code
    1. Add trang và chức năng liên hệ
    2. Chức năng bình luận sản phẩm

--- controllers/ChiTietController.php
+++ controllers/ChiTietController.php
@@ -1,13 +1,14 @@
 <?php
 
-function chitietsp($id){
+function chitietsp($id)
+{
 
     $view = 'chitiet';
 
     $dataDanhMuc = listAll('danhmuc');
     // Lấy thông tin sản phẩm hiện tại
     $dataSanPham = showOne2table('sanpham', 'anhsanpham', 'SanPhamID', 'ID_SanPham', 'SanPhamID', $id);
-    
+
     // Lấy ID_DanhMuc của sản phẩm hiện tại
     $idDanhMuc = $dataSanPham['ID_DanhMuc'];
 
@@ -15,6 +16,12 @@ function chitietsp($id){
     $SanPhamCungLoai = listMany('sanpham', 'anhsanpham', 'SanPhamID', 'ID_SanPham', 'ID_DanhMuc', $idDanhMuc);
     // debug($SanPhamCungLoai);
 
+    $SanPhamBinhLuan = listMany('binhluan', 'sanpham', 'idsanpham', 'SanPhamID', 'idsanpham', $id);
+
+    
+
+
+
+
     require_once PATH_VIEW . 'layouts/master.php';
 }
-
